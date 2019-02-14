5d444ebe56d9f297846898594b7e798400414346
christopher.larabee@mnhs.org <christopher.larabee@mnhs.org>
2019-02-14 16:13:10 -0600

    BI-64; Took a first pass at a handful of classes that map to Tessitura tables. Namely, T_SUB_LINEITEM, TR_PRICE_TYPE, and TR_SLI_STATUS. I established relationships between the classes. They're many-to-one relationships since the price_type and sli_status tables are just reference tables.

--- backend/tess_sqlalchemy.py
+++ backend/tess_sqlalchemy.py
@@ -1,10 +1,8 @@
-import csv
-import datetime
-from sqlalchemy import (create_engine, Column, Integer, String, Float, 
-                        DateTime, Date)
-from sqlalchemy.sql.expression import (func)
+from datetime import datetime as dt
+from sqlalchemy import (create_engine, ForeignKey, Column, Integer, String, 
+                        Numeric, DateTime, Date, Boolean)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine('sqlite:///:memory:', echo=False)
 Base = declarative_base()
@@ -12,11 +10,9 @@ Base = declarative_base()
 ## Declare models
 class AttendanceReport(Base):
     """
-    ===========================================================================
     An attendance count by site, ticket category, and day (as the current 
     smallest unit of measure in time that the organization can accomodate with
     the whole of our data).
-    ===========================================================================
     """
     __tablename__ = 'attendance_report'
     
@@ -25,7 +21,7 @@ class AttendanceReport(Base):
     category = Column(String) # Ticket category.
     date = Column(Date)
     count = Column(Integer)
-    revenue = Column(Float)
+    revenue = Column(Numeric)
     
     def __repr__(self):
         result = """<AttendanceReport(id='%d', 
@@ -41,168 +37,57 @@ class AttendanceReport(Base):
                                                          self.revenue)
         return result
     
-class TessAttendance(Base):
+class TessSubLineitem(Base):
     """
-    ===========================================================================
     A log of every event attendance event recorded in T_SUB_LINEITEM in the
     Tessitura database, with appropriate transformations to fit our business
     rules.
-    ===========================================================================
     """    
-    __tablename__ = 'tess_attendance'
+    __tablename__ = 'tess_sub_lineitem'
     
     id = Column(Integer, primary_key=True)
     sli_no = Column(Integer)
-    paid_amt = Column(Float)
+    paid_amt = Column(Numeric)
+    price_type = Column(Integer, ForeignKey('tess_price_type.id'))
+    create_dt = Column(DateTime)
+    last_update_dt = Column(DateTime)
+    sli_status = Column(Integer, ForeignKey('tess_sli_status.id'))
+    perf_no = Column(Integer)
     order_no = Column(Integer)
-    price_type_desc = Column(String)
-    price_type_short_desc = Column(String)
-    sli_status_desc = Column(String)
-    perf_dt = Column(DateTime)
-    order_dt = Column(DateTime)
-    price_type_category_desc = Column(String)
-    price_type_category_short_desc = Column(String)
-    perf_desc = Column(String)
-    prod_desc = Column(String)
-    perf_type_desc = Column(String)
-    season_desc = Column(String)
-    season_fy = Column(Integer)
-    mos_desc = Column(String)
-    site_code = Column(String)
-    gl_account_no = Column(String)
+    original_price_type = Column(Integer)
+    
+    ## Relationships:
+    sli_status_ref = relationship('TessSLIStatus', backref='tess_sli_status')
+    price_type_ref = relationship('TessPriceType', backref='tess_price_type')
     
     def __repr__(self):
-        result = """<TessAttendance(id='%d',
-                                sli_no='%d',
-                                paid_amt='%f', 
-                                order_no='%d', 
-                                price_type_desc='%s', 
-                                price_type_short_desc='%s',
-                                sli_status_desc='%s',
-                                perf_dt='%s',
-                                order_dt='%s',
-                                price_type_category_desc='%s',
-                                price_type_category_short_desc='%s',
-                                perf_desc='%s',
-                                prod_desc='%s',
-                                perf_type_desc='%s',
-                                season_desc='%s',
-                                season_fy='%d',
-                                mos_desc='%s',
-                                site_code='%s',
-                                gl_account_no='%s')>""" % (self.id,
-                                                           self.sli_no,
-                                                           self.paid_amt, 
-                                                           self.order_no, 
-                                                           self.price_type_desc, 
-                                                           self.price_type_short_desc,
-                                                           self.sli_status_desc,
-                                                           self.perf_dt,
-                                                           self.order_dt,
-                                                           self.price_type_category_desc,
-                                                           self.price_type_category_short_desc,
-                                                           self.perf_desc,
-                                                           self.prod_desc,
-                                                           self.perf_type_desc,
-                                                           self.season_desc,
-                                                           self.season_fy,
-                                                           self.mos_desc,
-                                                           self.site_code, 
-                                                           self.gl_account_no)
+        result = "<TessSubLineitem(id='%d', sli_no='%d')>" % (self.id, 
+                                                              self.sli_no)
         return result
-    
-    @classmethod
-    def import_csv(cls, session, file_name):
-        """
-        =======================================================================
-        Imports a properly formatted csv file into TessAttendance.
         
-        Currently this is configured to match the tess_orders.csvs in this
-        feature branch. It will need to be re-jiggered once we know what 
-        the Tessitura CSV files are going to look like going forward.
-        =======================================================================
-        """        
+class TessSLIStatus(Base):
+    __tablename__ = 'tess_sli_status'
+    
+    id = Column(Integer, primary_key=True)
+    status_desc = Column(String)
+    indicates_attendance = Column(Boolean)
+    
+    def __repr__(self):
+        result = "<TessSLIStatus(id='%d', status_desc='%s')>" % (self.id, 
+                                                                  self.status_desc)
+        return result
 
-        with open(file_name, 'r') as o:
-            dr = csv.DictReader(o)
-            to_db = [(i['sli_no'],
-                      i['paid_amt'],
-                      i['order_no'],
-                      i['price_type_desc'],
-                      i['price_type_short_desc'],
-                      i['sli_status_desc'],
-                      i['perf_dt'],
-                      i['order_dt'],
-                      i['price_type_category_desc'],
-                      i['price_type_category_short_desc'],
-                      i['perf_desc'],
-                      i['prod_desc'],
-                      i['perf_type_desc'],
-                      i['season_desc'],
-                      i['season_fy'],
-                      i['mos_desc'],
-                      i['site_code'],
-                      i['gl_account_no']) for i in dr]
-        
-        for data in to_db:
-            session.add(TessAttendance(sli_no=data[0],
-                                   paid_amt=data[1],
-                                   order_no=data[2],
-                                   price_type_desc=data[3],
-                                   price_type_short_desc=data[4],
-                                   sli_status_desc=data[5],
-                                   perf_dt=datetime.datetime.strptime(data[6],
-                                        '%m/%d/%Y %H:%M'),
-                                   order_dt=datetime.datetime.strptime(data[7],
-                                        '%m/%d/%Y %H:%M'),
-                                   price_type_category_desc=data[8],
-                                   price_type_category_short_desc=data[9],
-                                   perf_desc=data[10],
-                                   prod_desc=data[11],
-                                   perf_type_desc=data[12],
-                                   season_desc=data[13],
-                                   season_fy=data[14],
-                                   mos_desc=data[15],
-                                   site_code=data[16],
-                                   gl_account_no=data[17]
-                                   )    
-            )   
+class TessPriceType(Base):
+    __tablename__ = 'tess_price_type'
+
+    id = Column(Integer, primary_key=True)
+    price_type_desc = Column(String)
+    price_type_category = Column(Integer)
     
-        session.commit()
-    
-    @classmethod
-    def daily_attendance_report(cls, session, date):
-        """
-        =======================================================================
-        Return an attendance report for a given day. 
-        =======================================================================
-        """
-        ## BI-54: Update this so it can handle date inputs better.
-        date_obj = datetime.datetime.strptime(date, '%m/%d/%Y')
-        date_param = date_obj.strftime('%Y-%m-%d')
-        
-        result = (
-                session
-                .query(cls.site_code,
-                       cls.perf_dt,                           
-                       cls.price_type_category_desc,
-                       func.count(cls.sli_no),
-                       func.sum(cls.paid_amt))
-                .filter(func.date(cls.perf_dt) == date_param)
-                .group_by(cls.site_code, 
-                          func.date(cls.perf_dt),
-                          cls.price_type_category_desc)
-                .all()
-                )
-        return [
-                AttendanceReport(
-                        site_code=site_code,
-                        category=category,
-                        date=date,
-                        count=count,
-                        revenue=total)
-                for site_code, date, category, count, total in result
-                ]
+    def __repr__(self):
+        result = "<TessPriceType(id='%d', price_type_desc='%s')>" % (self.id, 
+                                                          self.price_type_desc)
+        return result
 
 def test_tess_objects():
     """
@@ -217,33 +102,62 @@ def test_tess_objects():
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    # Load the static dataset for testing.
-    TessAttendance.import_csv(session, 'tess_orders100.csv')
-
-    ## Test daily_attendance_report against a picked day.
-    session.add_all(TessAttendance.daily_attendance_report(session, 
-                                                           '3/19/2016'))
-    
+    ## Test examples:
+    session.add_all([TessSLIStatus(status_desc='Seated, Paid',
+                                   indicates_attendance=True),
+                     TessSLIStatus(status_desc='Ticketed, Paid',
+                                   indicates_attendance=True)
+        ])
+                     
     session.commit()
     
-    results = session.query(AttendanceReport).all()
+    session.add_all([TessPriceType(price_type_desc='Adult',
+                                   price_type_category=1),
+                     TessPriceType(price_type_desc='Comp',
+                                   price_type_category=2),
+                     TessPriceType(price_type_desc='Child (Under 5)',
+                                   price_type_category=1)
+    ])
     
-    ## Check the results of TessAttendance.daily_attendance_report for 3/19/2016.
-    ## There should be 3 row results (1 for each price type on that day), with
-    ## the following breakdown of count and revenue:
-    expected_agg_result = [[2, 0.0], [3, 12.0], [46, 276.0]]
-    
-    ## Compare expectation with results:
-    test_result = [False, False, False]
-    
-    for i, agg_result in enumerate(expected_agg_result):
-        if (expected_agg_result[i][0] == results[i].count and
-            expected_agg_result[i][1] == results[i].revenue):
-                test_result[i] = True
-                
-    return test_result
+    session.add_all([TessSubLineitem(sli_no=3109,
+                                     paid_amt=0.00,
+                                     price_type=3,
+                                     create_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     last_update_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     sli_status=1,
+                                     perf_no=143,
+                                     order_no=473819,
+                                     original_price_type=17),
+                     TessSubLineitem(sli_no=3110,
+                                     paid_amt=0.00,
+                                     price_type=2,
+                                     create_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     last_update_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     sli_status=1,
+                                     perf_no=143,
+                                     order_no=473820,
+                                     original_price_type=11),
+                     TessSubLineitem(sli_no=3111,
+                                     paid_amt=12.00,
+                                     price_type=1,
+                                     create_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     last_update_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     sli_status=2,
+                                     perf_no=248,
+                                     order_no=473823,
+                                     original_price_type=1),
+                     TessSubLineitem(sli_no=3112,
+                                     paid_amt=12.00,
+                                     price_type=1,
+                                     create_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     last_update_dt=dt.strptime('2016-03-10 15:16', '%Y-%m-%d %H:%M'),
+                                     sli_status=2,
+                                     perf_no=248,
+                                     order_no=473823,
+                                     original_price_type=1),                                     
+        ])
         
-    
+    session.commit()  
+        
 if __name__ == '__main__':
-    print(test_tess_objects())
-
+    test_tess_objects()
