47e3a5fba78e7638445988529620bd22e246674e
Richard <richard.tian@mnhs.org>
2019-03-29 15:47:03 -0500

    change backend endpoint

--- tff/chart.py
+++ tff/chart.py
@@ -14,7 +14,7 @@ def index():
 
 @blueprint.route('/attendance')
 def attendance():
-    url = "http://ec2-18-219-113-216.us-east-2.compute.amazonaws.com/monthly-attendance"
+    url = "http://ec2-18-222-170-180.us-east-2.compute.amazonaws.com/monthly-attendance"
     try:
         res = requests.get(url)
         return jsonify(res.json())
