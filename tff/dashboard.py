d9770645d78702861ce0caa3845c52e4fc5eba62
Richard Tian <richard.tian@mnhs.org>
2019-07-17 10:13:14 -0500

    code cleanup, ui improvement

--- tff/dashboard.py
+++ tff/dashboard.py
@@ -10,31 +10,6 @@ def index():
     )
 
 
-@blueprint.route('/dashboard/daily')
-def daily():
-    return render_template(
-        '/dashboard/chart_daily.html'
-    )
-
-
-@blueprint.route('/dashboard/weekly')
-def weekly():
-    return render_template(
-        '/dashboard/chart_weekly.html'
-    )
-
-@blueprint.route('/dashboard/quarterly')
-def quarterly():
-    return render_template(
-        '/dashboard/chart_quarterly.html'
-    )
-
-@blueprint.route('/dashboard/yearly')
-def yearly():
-    return render_template(
-        '/dashboard/chart_yearly.html'
-    )
-
 @blueprint.route('/dashboard/yoy')
 def yoy():
     return render_template(
