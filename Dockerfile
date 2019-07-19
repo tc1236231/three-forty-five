3c5840a79118be5b39885a7404f217fad083e856
Richard Tian <richard.tian@mnhs.org>
2019-07-19 16:12:45 -0500

    removed seaborn dependency, upgrade gcloud app engine config, chart default to month

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM tiangolo/uwsgi-nginx-flask:python3.7
-
-WORKDIR /app
-
-COPY . /app
-
-RUN pip install --trusted-host pypi.python.org -r requirements.txt
-
-ENV STATIC_PATH /app/tff/static
--- tff/chart.py
+++ tff/chart.py
@@ -1,15 +1,9 @@
-import io
-import random
-
 from flask import Blueprint, render_template, jsonify, Response
 from google.cloud import bigquery
-import seaborn as sns
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 blueprint = Blueprint('chart', __name__)
 
-project_name = 'mnhs-dw-test'
+project_name = 'mnhs-dw-prod'
 query_tables = {
     'yearly': '{}.dw.visits_yearly_full_padded_view'.format(project_name),
     'quarterly': '{}.dw.visits_quarterly_full_padded_view'.format(project_name),
@@ -54,25 +48,3 @@ def attendance(mode):
     except Exception as e:
         return jsonify('Error: ' + str(e)), 500
 
-
-def beeswarmplot():
-    sns.set(style="whitegrid")
-    tips = sns.load_dataset("tips")
-    ax = sns.swarmplot(x="day", y="total_bill", data=tips)
-    return ax.get_figure()
-
-
-@blueprint.route('/api/attendance/hourly/plot')
-def display():
-    fig = beeswarmplot()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
